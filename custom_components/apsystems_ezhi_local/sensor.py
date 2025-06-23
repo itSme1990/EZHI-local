46cec65672168a1f508f636341e577b221719f06
Nicolai Laaber <nicolai.laaber@gmail.com>
2025-06-23 16:38:32 +0200

    wron sensor updated

--- custom_components/apsystems_ezhi_local/sensor.py
+++ custom_components/apsystems_ezhi_local/sensor.py
@@ -239,7 +239,7 @@ class BatteryPowerSensor(BaseSensor):
         """Handle updated data from the coordinator."""
         if self.coordinator.data is not None:
             try:
-                self._state = float(self.coordinator.data.batP * -1)
+                self._state = float(self.coordinator.data.batP)
             except (ValueError, TypeError):
                 self._state = 0
         self.async_write_ha_state()
@@ -353,7 +353,7 @@ class OnGridPowerSensor(BaseSensor):
         """Handle updated data from the coordinator."""
         if self.coordinator.data is not None:
             try:
-                self._state = float(self.coordinator.data.ogP)
+                self._state = float(self.coordinator.data.ogP*-1)
             except (ValueError, TypeError):
                 self._state = 0
         self.async_write_ha_state()
